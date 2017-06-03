4f4c8943ca1ba56c52e8ca7a93048a17bccfbe79
wanghongwei <1018704480@qq.com>
2017-06-03 21:16:33 +0800

    two.java 中间插入输出语句

--- src/Two.java
+++ src/Two.java
@@ -28,6 +28,7 @@ public class Two {
     public static void main(String[] args) {
         int[] a ={1,5,9,10,13,19,20,26,28,63};
        System.out.println(Two.binary(a, 100));
+        System.out.println("这是我修改工程项目中的某一个文件内容，我现在要把他提交上去");
     }
 
    }
